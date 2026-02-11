9f8f99081b7ab0e562243ec5ab593b55b9b02d74
Jeyamurugan Palaniselvam <jyamurugan@gmail.com>
2026-02-12 01:10:27 +0530

    frontend: style update

--- frontend/src/lib/components/sidebar/SidebarList.svelte
+++ frontend/src/lib/components/sidebar/SidebarList.svelte
@@ -14,13 +14,14 @@
 			{#each collections as col}
 				<li>
 					<button
-						class="w-full px-4 py-2 text-left transition-colors {activeCollection &&
+						title={col}
+						class="w-full truncate px-4 py-2 text-left transition-colors {activeCollection &&
 						activeCollection.replace(/\\/g, '/') === col.replace(/\\/g, '/')
 							? 'preset-filled-primary-500'
 							: 'hover:bg-surface-500'}"
 						onclick={() => onselect(col)}
 					>
-						<span class="truncate">{col.split(/[\\/]/).pop()}</span>
+						<span>{col.split(/[\\/]/).pop()}</span>
 					</button>
 				</li>
 			{/each}
